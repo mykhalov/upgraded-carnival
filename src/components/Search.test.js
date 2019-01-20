e79b88ccbb8308935577dd6a48ab90fb9a5fd243
Pavlo Mykhalov <pmykhalov@gmail.com>
2019-01-20 14:43:57 +0200

    add component test

---
+++ src/components/Search.test.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Search from './Search'
+
+describe('Search component', () => {
+  test('should call onSubmit', () => {
+    const onSubmit = jest.fn()
+    const wrapper = shallow(<Search onSubmit={onSubmit} />)
+    const event = {
+      nativeEvent: {
+        target: {
+          username: {
+            value: 'octocat',
+          },
+        },
+      },
+      preventDefault: jest.fn(),
+    }
+
+    wrapper.find('form').simulate('submit', event)
+
+    expect(onSubmit).toHaveBeenCalledWith('octocat')
+    expect(event.preventDefault).toHaveBeenCalled()
+  })
+})
